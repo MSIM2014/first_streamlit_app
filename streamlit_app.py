c9e35744107e6ae74934194120231d0a57f8d4d4
MSIM2014 <wcao@uw.edu>
2022-07-29 22:51:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,11 +25,12 @@ def get_fruityvice(this_fruit_choice):
         return fruityvice_normalized
 
 def get_fruit_load_list():
-        with my_cnx.cursor() as my_cur:
-            my_cur.execute("select * from fruit_load_list")
-            return my_cur.fetchall()
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
 
 def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
         my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('new_fruit')")
         streamlit.write('Thanks for adding ', new_fruit)
 
