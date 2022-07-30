788f41c227820288a7f04b9a3791ee9464706b60
MSIM2014 <wcao@uw.edu>
2022-07-29 22:27:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ except URLError as e:
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
-        streamlit.dataframe(my_data_rows)
+    streamlit.dataframe(my_data_rows)
         
 # my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
